295fa809c8776d600872741c3c36cf5b4f086d31
Dmitry Astrikov <astrikov.d@gmail.com>
2018-08-08 15:16:05 +0700

    Small updates

--- backuper/core.py
+++ backuper/core.py
@@ -41,20 +41,25 @@ class Backuper:
         dt = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
         for database in self.settings['db']:
-            sql_filename = os.path.join(self.settings['tmp_dir'], '%s.dump.sql' % database["name"])
-            with open(sql_filename, 'w') as fd:
+            sql_filename = '%s.dump.sql' % database["name"]
+            sql_filepath = os.path.join(self.settings['tmp_dir'], sql_filename)
+            with open(sql_filepath, 'w') as fd:
                 call(
                     self.get_dump_args(database=database),
                     stdout=fd
                 )
 
-            zip_filename = os.path.join(self.settings['tmp_dir'], '%s_%s.zip' % (sql_filename, dt))
-            with open(sql_filename, 'r') as fd:
-                z = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
+            zip_filename = '%s_%s.zip' % (sql_filename, dt)
+            zip_filepath = os.path.join(self.settings['tmp_dir'], zip_filename)
+            with open(sql_filepath, 'r') as fd:
+                z = zipfile.ZipFile(zip_filepath, 'w', zipfile.ZIP_DEFLATED)
                 z.writestr(sql_filename, fd.read())
 
-            os.remove(sql_filename)
-            zips.append(zip_filename)
+            os.remove(sql_filepath)
+            zips.append({
+                'path': zip_filepath,
+                'name': zip_filename
+            })
 
         self.store(zips)
 
@@ -66,7 +71,7 @@ class Backuper:
             aws_secret_access_key=aws['secret_access_key']
         )
         bucket = s3.Bucket(aws['bucket'])
-        for zip_filename in zips:
-            with open(zip_filename, 'rb') as fd:
-                bucket.put_object(Key=zip_filename, Body=fd)
-            os.remove(zip_filename)
+        for zip_data in zips:
+            with open(zip_data['path'], 'rb') as fd:
+                bucket.put_object(Key=zip_data['name'], Body=fd)
+            os.remove(zip_data['path'])
